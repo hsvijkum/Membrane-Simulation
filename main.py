f66e354e40dd8b537febbef1bbc4ec456882d0f2
muntereiner <muntereiner15@gmail.com>
2018-10-06 00:54:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ for l in lipids:
 	for c in chol:
 		for a in asym:
 			subprocess.call(["./simulate.txt", l, str(c), str(a)])
-			f = open("ener-area-out.xvg")
+			f = open("files/ener-area-out.xvg")
 			ls = f.readlines()
 			f.close()
 
