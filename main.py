fb8d0324fad6f800fe4a785a13521fa1e5619e77
muntereiner <muntereiner15@gmail.com>
2018-10-06 02:28:51 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys,os,math,random,subprocess
 import numpy as np
 
-lipids=["DBPC", "DLPC", "DOPC", "DPPC", "DTPC", "DVPC", "DXPC", "DYPC"]
-chol=[5, 10, 15, 20, 25, 30]
-asym=[10, 20, 30]
+lipids=["DOPC", "DPPC", "DTPC"]
+chol=[30]
+asym=[30, 40]
+
+M = []
 
 for l in lipids:
 	for c in chol:
@@ -12,10 +14,11 @@ for l in lipids:
 			f = open("ener-area-out.xvg")
 			ls = f.readlines()
 			data = []
-			data.append(float(ls[3].split("Average +/- sd projected area per lipid:")[1]))
-			data.append(float(ls[4].split("Calculate compressibility moduleus: ave")[1]))
+			data.append(float(ls[3].split("Average +/- sd projected area per lipid: ")[1].split(" ")[0]))
+			data.append(float(ls[4].split("Calculate compressibility moduleus: ave ")[1].split(" ")[0]))
 			f.close()
-			print(data)
+			subprocess.call(["rm ener-area-out.xvg"])
+			M.append(data)
 
-
+print(M)
 sys.exit(0)
