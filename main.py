9624dfeb02c2d4710aa7db58f5cd092da27f60eb
LLNL Clinic <llnl@curie.Phys.HMC.Edu>
2018-10-10 14:09:50 -0700

    Update for initial sim

--- main.py
+++ main.py
@@ -2,24 +2,13 @@ import sys,os,math,random,subprocess
 import numpy as np
 import matplotlib
 
-lipids=["DBPC", "DLPC", "DOPC", "DPPC", "DTPC", "DVPC", "DXPC", "DYPC"]
-chol=[5, 10, 15, 20, 25, 30]
-asym=[10, 20, 30, 40]
-
-M = []
+lipids=["DOPC", "DPPC", "DTPC"]
+chol=[20, 30, 40]
+asym=[20, 30]
 
 for l in lipids:
 	for c in chol:
 		for a in asym:
 			subprocess.call(["./simulate.txt", l, str(c), str(a)])
-			f = open("analysis/ener-area-out.xvg")
-			ls = f.readlines()
-			data = []
-			data.append(float(ls[3].split("Average +/- sd projected area per lipid: ")[1].split(" ")[0]))
-			data.append(float(ls[4].split("Calculate compressibility moduleus: ave ")[1].split(" ")[0]))
-			f.close()
-			subprocess.call(["rm -r", "analysis"])
-			M.append(data)
 
-print(M)
 sys.exit(0)
